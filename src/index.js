4b8a79a90afbfb9066694ba9dfd5451be8d4e74a
Rayleigh <gmj1197@gmail.com>
2018-12-22 15:36:00 +0900

    fix: Modify redux function in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// createStore 와 루트 리듀서 불러오기
-import { createStore } from "redux";
-import rootReducer from "./store/modules";
+// src/redux/configureStore 불러오기
+import configureStore from "redux/configureStore";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 // 리덕스 개발자도구 적용
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
